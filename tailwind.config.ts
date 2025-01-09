931ca661559e9c1233d4339766d841345abdea0c
alvillage <azuknisianta@gmail.com>
2025-01-09 19:59:10 +0700

    ## - setup theme environment
    Signed-off-by: alvillage <azuknisianta@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    container: {
+      center: true,
+    },
+    colors: {
+      primary: "#4A4A4A",
+      secondary: "#696970",
+      ternary:"#9999A1",
+      contras: "#F4F4F6",
+      accent: "#E6E6E9",
+      // info: "#BBDDF4",
+      // warning: "#FACD2B",
+      // success: "#26FF2A",
+      // danger: "#FF213F",
+    }
   },
   plugins: [],
 } satisfies Config;
